0dd455de2b2a784ef1c10abe4ad6823156afecbd
Pinkie Pie <yarnovax@gmail.com>
2017-12-15 10:53:50 -0500

    Update for versioning

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = function AutoVanguard(dispatch) {
-	dispatch.hook('S_COMPLETE_EVENT_MATCHING_QUEST', event => {
-		dispatch.toServer('C_COMPLETE_DAILY_EVENT', { id: event.id })
+	dispatch.hook('S_COMPLETE_EVENT_MATCHING_QUEST', 1, event => {
+		dispatch.toServer('C_COMPLETE_DAILY_EVENT', 1, { id: event.id })
 		return false
 	})
 }
